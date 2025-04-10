280bf9f405275e01d5e2059bdf37db5e6db29a47
Mailan Franco <36058994+thesckurtt@users.noreply.github.com>
2025-04-10 16:12:54 -0300

    Update MainCadastroLandingPage.jsx

--- src/components/home/MainCadastroLandingPage.jsx
+++ src/components/home/MainCadastroLandingPage.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import ButtonDuoSuccess from "../ButtonDuoSuccess";
 
-const MainCadastroLandingPage = () => {
+const MainCadastroLandingPage = ({login}) => {
+  // const persons = [
+  //   { id: "1", img: "./img/avatar_01.svg", active: false },
+  //   { id: "2", img: "./img/avatar_02.svg", active: false },
+  //   { id: "3", img: "./img/avatar_03.svg", active: false },
+  // ];
+
+  const [avatar, setAvatar] = useState([
+    { id: "1", img: "./img/avatar_01.svg", active: false },
+    { id: "2", img: "./img/avatar_02.svg", active: false },
+    { id: "3", img: "./img/avatar_03.svg", active: false },
+  ]);
+  
+  function toggleAvatarActive(id) {
+    const updated = avatar.map((person) => ({
+      ...person,
+      active: person.id === id,
+    }));
+    setAvatar(updated);
+  }
+
   return (
     <main className="main-site-lp limit_grid_sm">
       <div className="container max-500">
@@ -14,7 +34,22 @@ const MainCadastroLandingPage = () => {
             <div className="choice-avatar mb-4">
               <span className="fs-4 font-din-bold">Escolha seu avatar</span>
               <div className="container-choice-avatar d-flex gap-4 mt-3">
-                <div className="container-choice-avatar-img">
+                {avatar.map((person) => {
+                  return (
+                    <div
+                      key={person.id}
+                      onClick={() => {
+                        toggleAvatarActive(person.id);
+                      }}
+                      className={`container-choice-avatar-img ${
+                        person.active ? "active" : ""
+                      }`}
+                    >
+                      <img src={person.img} alt="" />
+                    </div>
+                  );
+                })}
+                {/* <div className="container-choice-avatar-img">
                   <img src="./img/avatar_01.svg" alt="" />
                 </div>
                 <div className="container-choice-avatar-img">
@@ -22,7 +57,7 @@ const MainCadastroLandingPage = () => {
                 </div>
                 <div className="container-choice-avatar-img">
                   <img src="./img/avatar_03.svg" alt="" />
-                </div>
+                </div> */}
               </div>
             </div>
             <div className="choice-username d-flex flex-column">
