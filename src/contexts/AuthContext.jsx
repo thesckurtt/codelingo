fb633c563477c18499d2332e15cea1e7ebfe294e
Mailan Franco <36058994+thesckurtt@users.noreply.github.com>
2025-04-10 16:14:09 -0300

    Update AuthContext.jsx

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -23,7 +23,7 @@ export const AuthProvider = ({ children }) => {
   };
 
   return (
-    <AuthContext.Provider value={{ isLoggedIn, login, logout }}>
+    <AuthContext.Provider value={{ isLoggedIn, login, logout, user }}>
       {children}
     </AuthContext.Provider>
   );
