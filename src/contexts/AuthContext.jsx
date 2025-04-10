3657797cd811a69030cfc5c55b5adcc610bb5343
Mailan Franco <36058994+thesckurtt@users.noreply.github.com>
2025-04-10 16:12:55 -0300

    Create AuthContext.jsx

---
+++ src/contexts/AuthContext.jsx
@@ -0,0 +1,32 @@
+import { createContext, useContext, useState } from "react";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(
+    localStorage.getItem("isLoggedIn") === "true"
+  );
+  const [user, setUser] = useState(null);
+
+  const login = (user) => {
+    localStorage.setItem('user', JSON.stringify(user))
+    localStorage.setItem("isLoggedIn", "true");
+    setIsLoggedIn(true);
+    setUser(user)
+  };
+
+  const logout = () => {
+    localStorage.removeItem("user");
+    localStorage.removeItem("isLoggedIn");
+    setIsLoggedIn(false);
+    setUser(null)
+  };
+
+  return (
+    <AuthContext.Provider value={{ isLoggedIn, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
