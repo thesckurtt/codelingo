56a5dde90bb96d56af04308a7f45e1940a628b2a
Mailan Franco <36058994+thesckurtt@users.noreply.github.com>
2025-04-10 16:46:44 -0300

    Update IndexApp.jsx

--- src/pages/app/IndexApp.jsx
+++ src/pages/app/IndexApp.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { useAuth } from '../../contexts/AuthContext'
 
 const IndexApp = () => {
+  // const {user} = useAuth();
   return (
     <div>
-      <h1>Hello, from App</h1>
+    <h1>Helllo</h1>
+      {/* <h1>Hello {user.name}, from App</h1> */}
     </div>
   )
 }
