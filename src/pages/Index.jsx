59798239985f0804eb692d2a0b960f19051db65e
Mailan Franco <36058994+thesckurtt@users.noreply.github.com>
2025-04-09 23:29:03 -0300

    Create Index.jsx

---
+++ src/pages/Index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../components/Header";
+import MainSiteLandingPage from "../components/home/MainSiteLandingPage";
+
+const index = () => {
+  return (
+    <>
+      <Header />
+      <MainSiteLandingPage />
+    </>
+  );
+};
+
+export default index;
