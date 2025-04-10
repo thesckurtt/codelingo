9dbe9270c0d31683bc038976f67376669f789c21
Mailan Franco <36058994+thesckurtt@users.noreply.github.com>
2025-04-09 23:51:31 -0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
     <header className="header-site-lp border-bottom-md d-flex align-items-center">
       <div className="limit_grid_sm d-flex justify-content-start px-4">
         <div className="img-container-header-site-lp">
-          <img src="./img/logo_v1.svg" alt="logo codelingo" />
+          <img src="./img/logo_v1.svg" alt="logo codelingo" draggable="false"/>
         </div>
       </div>
     </header>
