7ab5d156bda4c0fb86f43abe837e70178cb875ad
Mailan Franco <36058994+thesckurtt@users.noreply.github.com>
2025-04-09 23:51:38 -0300

    Create Cadastro.jsx

---
+++ src/pages/home/Cadastro.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../../components/Header'
+import MainCadastroLandingPage from '../../components/home/MainCadastroLandingPage'
+
+const cadastro = () => {
+  return (
+    <>
+      <Header />
+      <MainCadastroLandingPage />
+    </>
+  )
+}
+
+export default cadastro
