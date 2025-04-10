a795ce957658b344e6f1bac581cc1d995ce635ab
Mailan Franco <36058994+thesckurtt@users.noreply.github.com>
2025-04-09 23:51:29 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
-import Index from "./pages";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Index from "./pages/home/Index";
+import Cadastro from "./pages/home/Cadastro";
+
 function App() {
   return (
-    <Index />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/cadastro" element={<Cadastro />} />
+        <Route path="*" element={<Index />} />
+      </Routes>
+    </Router>
   );
 }
+// {/* <Index /> */}
 
 export default App;
