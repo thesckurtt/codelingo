7a3ad8e223f51b1db013150f38c2e87f320e5b8b
Mailan Franco <36058994+thesckurtt@users.noreply.github.com>
2025-04-09 23:29:00 -0300

    Create Header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className="header-site-lp border-bottom-md d-flex align-items-center">
+      <div className="limit_grid_sm d-flex justify-content-start px-4">
+        <div className="img-container-header-site-lp">
+          <img src="./img/logo_v1.svg" alt="logo codelingo" />
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
