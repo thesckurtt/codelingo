d83136e520f31df0000a6994d0cad29fc9bd6c9b
Mailan Franco <36058994+thesckurtt@users.noreply.github.com>
2025-04-10 23:16:19 -0300

    Update IndexApp.jsx

--- src/pages/app/IndexApp.jsx
+++ src/pages/app/IndexApp.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
-import { useAuth } from '../../contexts/AuthContext'
+import React from "react";
+import { useAuth } from "../../contexts/AuthContext";
+import SidebarApp from "../../components/app/SidebarApp";
+import MainDashboardApp from "../../components/app/MainDashboardApp"
 
 const IndexApp = () => {
-  const {user} = useAuth();
-  return (
-    <div>
-      <h1>Hello {user.username}, from App</h1>
-    </div>
-  )
-}
+  document
+    .querySelector("body")
+    .setAttribute("class", "d-flex bg-dashboard-color body-dashboard min-vh-100");
 
-export default IndexApp
+    document.getElementById('root').setAttribute('class', 'd-flex bg-dashboard-color body-dashboard w-100 min-vh-100')
+  const { user } = useAuth();
+  return (
+    <>
+      <SidebarApp/>
+      <MainDashboardApp />
+    </>
+  );
+};
+
+export default IndexApp;
