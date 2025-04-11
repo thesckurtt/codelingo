57701907cbb24b4c36455b861c616dd86bb4ffe4
Mailan Franco <36058994+thesckurtt@users.noreply.github.com>
2025-04-10 23:16:20 -0300

    Create usePersistentState.jsx

---
+++ src/utils/usePersistentState.jsx
@@ -0,0 +1,14 @@
+import { useState, useEffect } from "react";
+export default function usePersistentState(key, initialValue) {
+  const [value, setValue] = useState(() => {
+    const stored = localStorage.getItem(key);
+    return stored !== null ? JSON.parse(stored) : initialValue;
+  });
+  
+  useEffect(() => {
+    console.log('aqui')
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+
+  return [value, setValue];
+}
