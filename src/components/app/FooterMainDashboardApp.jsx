a2ec03726357b7de17ac3a4ef065c552a77a8ec9
Mailan Franco <36058994+thesckurtt@users.noreply.github.com>
2025-04-12 18:09:52 -0300

    Update FooterMainDashboardApp.jsx

--- src/components/app/FooterMainDashboardApp.jsx
+++ src/components/app/FooterMainDashboardApp.jsx
@@ -13,15 +13,24 @@ const FooterMainDashboardApp = ({statusFooter, btnLabel, question_id, moduleId, 
     case "correct":
       return (
         <div className="footer-main-dashboard-app bg-footer-correct-app border-top-sm w-100 p-3 d-flex align-items-center justify-content-between px-4 bg-">
-          <span className=" text-footer-correct font-din-bold fs-3">
-            <i class="fa-solid fs-5 p-2 rounded-5 bg-dashboard-color fa-check px-4 mx-2"></i>{" "}
+          <span className="text-footer-correct font-din-bold fs-3">
+            <i class="fa-solid fs-5 p-2 rounded-5 bg-dashboard-color fa-circle-check px-4 mx-2"></i>{" "}
             Resposta Correta
           </span>
           <button className="btn-duo btn-success text-dark" onClick={(e)=>{handleClick(e, moduleId, question_id)}}>{btnLabel}</button>
         </div>
       );
     case "incorrect":
-      return;
+      return (
+        <div className="footer-main-dashboard-app bg-footer-correct-app border-top-sm w-100 p-3 d-flex align-items-center justify-content-between px-4 bg-">
+          <span className="text-footer-incorrect font-din-bold fs-3">
+            {/* <i class="fa-solid fs-5 p-2 rounded-5 bg-dashboard-color fa-check px-4 mx-2"></i>{" "} */}
+            <i class="fa-solid fa-circle-xmark fs-5 p-2 rounded-5 bg-dashboard-color px-4 mx-2"></i>{" "}
+            Resposta Incorreta
+          </span>
+          <button className="btn-duo btn-success text-dark" onClick={(e)=>{handleClick(e, moduleId, question_id)}}>{btnLabel}</button>
+        </div>
+      );
     default:
       return;
   }
