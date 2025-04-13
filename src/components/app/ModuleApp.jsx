1e84eafceb00556ee32a3b07bdfab76c9f86a61e
Mailan Franco <36058994+thesckurtt@users.noreply.github.com>
2025-04-12 21:29:37 -0300

    Update ModuleApp.jsx

--- src/components/app/ModuleApp.jsx
+++ src/components/app/ModuleApp.jsx
@@ -4,24 +4,59 @@ import TextContentMainDashboardApp from "./TextContentMainDashboardApp";
 import AnswersContentMainDashboardApp from "./AnswersContentMainDashboardApp";
 import FooterMainDashboardApp from "./FooterMainDashboardApp";
 
-const ModuleApp = ({ module, initModule }) => {
+const ModuleApp = ({ module, initModule, setAppModules, appModules }) => {
   const [statusFooter, setStatusFooter] = useState("default"); // Footer do Main App [default - correct - incorrect]
   const [valueNowProgressBar, setValueNowProgressBar] = useState(0);
-  
+  const [selectedAnswer, setSelectedAnswer] = useState(0);
+  const [moduleQuestions, setModuleQuestions] = useState(module.questions)
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  // console.log(module.questions);
 
   const isIntro = module.initial_view;
 
-  function answerHandleClick(e, correctAnswer, answerOption) {
+  // Função para verificar as perguntas
+  function answerHandleClick(
+    e,
+    correctAnswer,
+    answerOption,
+    questionsLenght,
+    questionId
+  ) {
     document.querySelectorAll(".li-answer.active").forEach((el) => {
       el.classList.remove("active");
     });
 
     e.currentTarget.classList.add("active");
 
-    answerOption === correctAnswer
-      ? setStatusFooter("correct")
-      : setStatusFooter("incorrect");
-  } // Função para verificar as perguntas
+    setValueNowProgressBar((questionId / questionsLenght) * 100);
+
+    setSelectedAnswer(answerOption);
+
+    if (answerOption === correctAnswer) {
+      setStatusFooter("correct");
+    } else {
+      setStatusFooter("incorrect");
+    }
+  }
+
+  // Função para avançar para a próxima pergunta
+  function nextQuestion() {
+    // console.log(selectedAnswer)
+    const updated = moduleQuestions.map((question, index) => {
+      if(question.visibility == true){
+        question.visibility = false
+        console.log(question.title)
+      }
+      if(question.id == (currentQuestion + 2)){
+        question.visibility = true
+      }
+      // console.log(question.title, `  --  ${index}`)
+      return question;
+    })
+    // alert("avançar pergunta");
+    console.log(updated)
+    setStatusFooter('default')
+  }
 
   return (
     <main className="flex-grow-1 main-dashboard-app d-flex flex-column">
@@ -37,9 +72,9 @@ const ModuleApp = ({ module, initModule }) => {
         {!isIntro && (
           <>
             <ProgressBarApp valueNowProgressBar={valueNowProgressBar} />
-            {module.questions
+            {moduleQuestions
               .filter((q) => q.visibility == true)
-              .map((question, module) => {
+              .map((question) => {
                 return (
                   <React.Fragment key={question.id}>
                     <TextContentMainDashboardApp
@@ -47,7 +82,9 @@ const ModuleApp = ({ module, initModule }) => {
                       text={question.text}
                     />
                     <AnswersContentMainDashboardApp
-                      questionsLenght={module.questions}
+                      setSelectedAnswer={setSelectedAnswer}
+                      questionsLenght={module.questions.length}
+                      questionId={question.id}
                       answers={question.answers}
                       correctAnswer={question.correct_answer}
                       handleClick={answerHandleClick}
@@ -61,7 +98,9 @@ const ModuleApp = ({ module, initModule }) => {
       <FooterMainDashboardApp
         moduleId={module.id}
         isIntro={isIntro}
-        {...(isIntro ? { handleClick: initModule } : {})}
+        {...(isIntro
+          ? { handleClick: initModule }
+          : { handleClick: nextQuestion })}
         statusFooter={statusFooter}
         btnLabel={isIntro ? "Iniciar" : "Próxima"}
       />
