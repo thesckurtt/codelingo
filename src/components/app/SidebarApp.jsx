12493440be61460c60443856eda489cbf360e382
Mailan Franco <36058994+thesckurtt@users.noreply.github.com>
2025-04-14 11:50:31 -0300

    Update SidebarApp.jsx

--- src/components/app/SidebarApp.jsx
+++ src/components/app/SidebarApp.jsx
@@ -2,35 +2,15 @@
 import UlButtonSidebarApp from "./UlButtonSidebarApp";
 import { useState } from "react";
 
-const SidebarApp = ({setModuleNow}) => {
-  const [buttons, setButtons] = useState([
-    { label: "Introdução", active: false, module_id: 1 },
-    { label: "Módulo 1", active: false, module_id: 2 },
-    // { label: "Módulo 2", active: false, module_id: 3 },
-    // { label: "Perfil", active: false, module_id: 4 },
-  ]);
+const SidebarApp = ({setModuleNow, handleClick, buttons}) => {
 
-  function handleClick(e) {
-    // console.log(e.target.innerText)
-    const updated = buttons.map((button) => {
-      if (
-        button.label.toLowerCase() == e.target.innerText.trim().toLowerCase()
-      ) {
-        button.active = true;
-        setModuleNow(button.module_id)
-        return button;
-      } else {
-        button.active = false;
-        return button;
-      }
-    });
-    setButtons([...updated]);
-  }
   const btns = buttons.map((bt) => ({ ...bt, action: handleClick }));
 
   return (
-    <div className="sidebar-dashboard-app max-345 w-100 min-vh-100 p-2 border-end-sm d-flex align-items-center flex-column">
-      <div className="img-container-sidebar-dashboard-app">
+    <div className="sidebar-dashboard-app max-345 w-100 min-vh-100 p-2 border-end-sm d-flex align-items-center flex-column" >
+      <div style={{cursor: 'pointer'}} onClick={()=>{
+        window.location.reload()
+      }} className="img-container-sidebar-dashboard-app">
         <img src="./img/logo_dashboard.svg" alt="logo codelingo" />
       </div>
       <ul className="ul-sidebar-dashboard-app d-flex flex-column">
