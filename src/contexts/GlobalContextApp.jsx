2745bd554c3d3a03262371006642b557d5e11096
Mailan Franco <36058994+thesckurtt@users.noreply.github.com>
2025-04-23 08:56:24 -0300

    Update GlobalContextApp.jsx

--- src/contexts/GlobalContextApp.jsx
+++ src/contexts/GlobalContextApp.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const GlobalAppContext = createContext();
 
@@ -6,18 +6,21 @@ export const GlobalAppProvider = ({ children }) => {
   const [correctQuestion, setCorrectQuestion] = useState(0);
   const [wrongQuestion, setWrongQuestion] = useState(0);
 
+  function verifyTotalQuestions() {
+    console.log('questao correta: ', correctQuestion)
+    console.log('questao errada: ', wrongQuestion)
+  }
+  
   function handleCorrectAnswer() {
     setCorrectQuestion((correctQuestion) => correctQuestion + 1)
-    console.log(correctQuestion)
   }
 
   function handleWrongAnswer() {
     setWrongQuestion((wrongQuestion) => wrongQuestion + 1)
-    console.log(wrongQuestion)
   }
 
   return (
-    <GlobalAppContext.Provider value={{ correctQuestion, handleCorrectAnswer, wrongQuestion, handleWrongAnswer }}>
+    <GlobalAppContext.Provider value={{ correctQuestion, handleCorrectAnswer, verifyTotalQuestions, wrongQuestion, handleWrongAnswer }}>
       {children}
     </GlobalAppContext.Provider>
   )
